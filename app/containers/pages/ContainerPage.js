ee42a4b13449fa02db4ac306d7fc5ef80be6d1d4
j8seangel <jseangel@gmail.com>
2016-11-10 16:37:10 +0100

    Fix lint

--- app/containers/pages/ContainerPage.js
+++ app/containers/pages/ContainerPage.js
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux';
 import ContainerPage from 'components/pages/ContainerPage';
-import { setLanguage } from 'redux-i18n';
 
 const mapStateToProps = (state, { params }) => ({
   lang: params.lang
 });
+
 const mapDispatchToProps = () => ({});
 
 export default connect(mapStateToProps, mapDispatchToProps)(ContainerPage);
