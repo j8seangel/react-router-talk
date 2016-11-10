cc94bddd5041c4d8b9bc39e98bcb524d3b800889
j8seangel <jseangel@gmail.com>
2016-11-10 01:11:17 +0100

    Fix translations

--- app/components/pages/HomePage.js
+++ app/components/pages/HomePage.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import NavLink from 'containers/common/NavLink';
 
-function HomePage() {
+function HomePage(props, context) {
   return (
     <div className="row align-middle c-home">
       <div className="column">
-        <h1>Welcome to the React Router Redux example</h1>
+        <h1>{context.t('welcomeTo')}</h1>
         <h2><NavLink to="/team" i18nText="goToTheTeam" /></h2>
       </div>
     </div>
   );
 }
+
+// Example of getting something from the context
+// In this case the global function to translate
+HomePage.contextTypes = {
+  t: React.PropTypes.func.isRequired
+};
 
 export default HomePage;
--- app/locales/translations.js
+++ app/locales/translations.js
@@ -3,12 +3,14 @@ export const translations = {
     'hi': 'Hi',
     'home': 'Home',
     'team': 'Team',
-    'goToTheTeam': 'Go to the team !'
+    'goToTheTeam': 'Go to the team !',
+    'welcomeTo': 'Welcome to the React Router Redux example'
   },
   es: {
     'hi': 'Hola',
     'home': 'Inicio',
     'team': 'Equipo',
-    'goToTheTeam': 'Ir al equipo !'
+    'goToTheTeam': 'Vamos al equipo !',
+    'welcomeTo': 'Bienvenido al ejemplo de React Router Redux'
   },
 }
